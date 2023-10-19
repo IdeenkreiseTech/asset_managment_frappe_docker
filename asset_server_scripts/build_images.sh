ead6eb71645113518c8fc93bb276bacb377c67a8
SagarIdeen <isagarideen@gmail.com>
2023-10-19 13:30:49 +0530

    Server Scripts Updated

--- asset_server_scripts/build_images.sh
+++ asset_server_scripts/build_images.sh
@@ -1,12 +1,12 @@
 git_password=123
+TAG=2.0.14
 
 cd ../
-yes|docker builder prune --all
+yes|sudo docker builder prune --all
 
 # Create the JSON string with proper formatting
-app1='{"url": "https://github.com/frappe/erpnext","branch": "v13.52.1"}'
-app2='{"url": "https://IdeenkreiseTech:'"$git_password"'@github.com/IdeenkreiseTech/hybrid_90plus_app.git","branch": "payment-update"}'
-export APPS_JSON='['"$app1"', '"$app2"']'
+app1='{"url": "https://IdeenkreiseTech:'"$git_password"'@github.com/IdeenkreiseTech/asset_management.git","branch": "main"}'
+export APPS_JSON='['"$app1"']'
 export APPS_JSON_BASE64=$(echo ${APPS_JSON} | base64 -w 0)
 
 sudo docker build \
@@ -15,9 +15,9 @@ sudo docker build \
   --build-arg=PYTHON_VERSION=3.9.17 \
   --build-arg=NODE_VERSION=14.21.3 \
   --build-arg=APPS_JSON_BASE64=$APPS_JSON_BASE64 \
-  --tag=ghcr.io/ideenkreisetech/hybrid_90plus_app/hybrid_90plus_app:1.0.0 \
+  --tag=ghcr.io/anoopideen/asset_management/asset_management:$TAG \
   --file=images/custom/Containerfile .
 
 sudo docker login ghcr.io -u IdeenkreiseTech -p $git_password
 
-sudo docker push ghcr.io/ideenkreisetech/hybrid_90plus_app/hybrid_90plus_app:1.0.0
+sudo docker push ghcr.io/ideenkreisetech/nitta_note_app/nitta_note_app:$TAG
